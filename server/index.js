7c6617b86041f8fc904f1d96a5f5e1910d5f4e50
Celina Louissaint <celinalouissaint@gmail.com>
2024-03-03 13:23:05 -0800

    remove rewrite

--- server/index.js
+++ server/index.js
@@ -1,53 +1,53 @@
-// import { ApolloServer } from "@apollo/server";
-// import { expressMiddleware } from "@apollo/server/express4";
-// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-// import express from "express";
-// import http from "http";
-// import cors from "cors";
-// import bodyParser from "body-parser";
-// import { schemas } from "./schemas/index.js";
-// import { authMiddleware } from "./utils/auth.js";
-// import { runDBClient } from "./connection/index.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { schemas } from "./schemas/index.js";
+import { authMiddleware } from "./utils/auth.js";
+import { runDBClient } from "./connection/index.js";
 
 
-// const PORT = 4000;
-// const app = express();
-// const httpServer = http.createServer(app);
-// const { typeDefs, resolvers } = schemas;
+const PORT = 4000;
+const app = express();
+const httpServer = http.createServer(app);
+const { typeDefs, resolvers } = schemas;
 
-// const serverListen = async (PORT) => {
-//   httpServer.listen({ port: PORT })
-// }
+const serverListen = async (PORT) => {
+  httpServer.listen({ port: PORT })
+}
 
-// const StartApolloServer = async () => {
+const StartApolloServer = async () => {
   
-//   console.log(`Starting Server`);
+  console.log(`Starting Server`);
 
-//   const server = new ApolloServer({
-//     // The GraphQL schema
-//     typeDefs,
-//     // A map of functions which return data for the schema.
-//     resolvers,
-//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-//   });
+  const server = new ApolloServer({
+    // The GraphQL schema
+    typeDefs,
+    // A map of functions which return data for the schema.
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-//   await server.start().catch(console.dir);
+  await server.start().catch(console.dir);
 
-//   app.use(
-//     cors(),
-//     bodyParser.json(),
-//     expressMiddleware(server, {
-//       context: authMiddleware,
-//     })
-//   );
-//   return console.log(`🚀 Server ready at ${PORT}`);
-// };
+  app.use(
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+  return console.log(`🚀 Server ready at ${PORT}`);
+};
 
-// runDBClient();
-// StartApolloServer();
-// serverListen(PORT);
+runDBClient();
+StartApolloServer();
+serverListen(PORT);
 
-// export default StartApolloServer;
+export default StartApolloServer;
 
 
 
