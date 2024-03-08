0e384c0b033a7e6c2ae0e75c6fa9b6bfbc4799b6
Celina Louissaint <celinalouissaint@gmail.com>
2024-03-08 12:50:29 -0800

    add logic to get load time

--- server/api/index.js
+++ server/api/index.js
@@ -13,9 +13,16 @@ const app = express();
 export const httpServer = http.createServer(app);
 const { typeDefs, resolvers } = schemas;
 const PORT = process.env.PORT || 4000;
+let initTime = new Date().getTime();
 
 const serverListen = (PORT) => {
+  let loadTime = new Date().getTime();
+  let timeToLoad = loadTime - initTime;
+
+  console.log("----------------",timeToLoad)
+
   httpServer.listen({ port: PORT });
+
   if (!PORT) {
     throw new Error("No Port Set!");
   }
