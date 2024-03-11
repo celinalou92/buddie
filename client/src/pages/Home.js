220c7225c8851d5d27f3a47486d86aa702c4c59c
Celina Louissaint <celinalouissaint@gmail.com>
2024-03-11 16:58:41 -0700

    Refactor page to show loggedIn message when user is not logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
 //bring in QUERY_ME_BASIC when needed
-import { QUERY_MESSAGES, QUERY_TASKS, QUERY_ME } from "../utils/queries"; 
+import { QUERY_MESSAGES, QUERY_TASKS, QUERY_ME } from "../utils/queries";
 // import FriendList from "../components/FriendList";
 // import TaskForm from '../components/TaskForm';
 import Auth from "../utils/auth";
@@ -14,46 +14,49 @@ import Grid from "@material-ui/core/Grid";
 const Home = () => {
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_MESSAGES);
-    // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
   const { data: userData } = useQuery(QUERY_ME);
   // const { waiting, Taskdata } = useQuery(QUERY_TASKS);
-  
+
   // optional chaining syntax used below
   const messages = data?.messages || [];
 
   const loggedIn = Auth.loggedIn();
 
+  if (!loggedIn || undefined) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
+      </h4>
+    );
+  }
   const tasks = userData?.me.tasks || [];
-
 
   return (
     <main>
       <Grid direction="row" container spacing={2}>
-        <Grid  item sm={6} >
+        {/* <Grid  item sm={6} >
         <h2 className="heading">My Tasks</h2>
           <div className={`${loggedIn } flex-row compBorders scroller`} >
-          
           <DashTask tasks={tasks} />
-
           </div>
-        
-          
-        </Grid>
-        <Grid item sm={6} >
+        </Grid> */}
+        <Grid item sm={6}>
           <div className=" ">
             <h2 className="heading">Messages</h2>
-            <div className={` ${loggedIn }`}>
+            <div className={` ${loggedIn}`}>
               {loading ? (
                 <div>Loading...</div>
               ) : (
-                <div className="compBorders scroller">
-                  <MessageList messages={messages} />
-                </div>
-              )}
-              {loggedIn && (
-                <div className="col-12 mb-3" >
-                  <MessageForm />
-                </div>
+                <>
+                  <div className="compBorders scroller">
+                    <MessageList messages={messages} />
+                  </div>
+                  <div className="col-12 mb-3">
+                    <MessageForm />
+                  </div>
+                </>
               )}
               {/* {loggedIn && userData ? (
                 <div className="col-lg-3 mb-3">
